5a20d640ed16eb578ad43fbb0ac552f9848b0e98
웹스토리보이 <webstoryboy@naver.com>
2024-11-04 09:25:49 +0900

    2024.11.04

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>dd</div>;
+  return (
+    <>
+      <header className="max-w-screen-2xl mx-auto p-4">
+        <Link href="/">SpopStarWorld</Link>
+      </header>
+    </>
+  );
 }
